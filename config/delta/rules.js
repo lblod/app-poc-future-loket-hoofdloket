55b7f343892433edd534d9ea81f4aaac0d80f5cd
Erika Pauwels <erika.pauwels@gmail.com>
2024-04-26 11:32:33 +0200

    Add LDES consumer service for IPDC feed

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,7 +1,7 @@
 export default [
   {
     match: {
-
+      predicate: { type: 'uri', value: 'http://purl.org/dc/terms/isVersionOf' }
     },
     callback: {
       url: "http://resource/.mu/delta",
@@ -10,6 +10,22 @@ export default [
     options: {
       resourceFormat: "v0.0.1",
       gracePeriod: 500,
+      foldEffectiveChanges: true,
+      ignoreFromSelf: true
+    }
+  },
+  {
+    match: {
+
+    },
+    callback: {
+      url: "http://ldes-object/delta",
+      method: "POST"
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 500,
+      foldEffectiveChanges: true,
       ignoreFromSelf: true
     }
   }
