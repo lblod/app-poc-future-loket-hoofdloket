c406d9efb64cf40d0c24b240a9f0e7757c406d57
Erika Pauwels <erika.pauwels@gmail.com>
2024-04-29 11:00:20 +0200

    Add dispatcher rule for concepts

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -29,6 +29,10 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://resource/public-services/"
   end
 
+  get "/concepts/*path", @json do
+    Proxy.forward conn, path, "http://resource/concepts/"
+  end
+
   match "/*_", %{ layer: :not_found } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
